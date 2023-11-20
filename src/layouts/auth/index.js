f0712876670cad17053112b8282cde641d4ffd60
Bobur Yusupov <bobur.yu@anur.tech>
2023-11-20 10:17:00 +0500

    Auth Poster completed

--- src/layouts/auth/index.js
+++ src/layouts/auth/index.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import "./auth.css"
 
 // Pages
 import Login from "../../views/auth/login";
@@ -6,14 +7,19 @@ import Signup from "../../views/auth/signup";
 
 const AuthLayout = () => {
     return (
-        <>
-            <h1>Work</h1>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-            </Routes>
+        <div className="Auth">  
+            <div className="Auth-Poster">
+                <p>Inspired by the future:</p>
+                <h1 className="Auth-Poster-Title">THE Dashboard-app by dev-yusupov</h1>
+            </div>
+            <div className="Auth-Form">
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                </Routes>
+            </div>
         
-        </>
+        </div>
     )
 }
 
