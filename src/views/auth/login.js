75346c6d3c1990852c03b599c3921eddefd6617a
Bobur Yusupov <bobur.yu@anur.tech>
2023-11-20 11:38:04 +0500

    Login Page Completed

--- src/views/auth/login.js
+++ src/views/auth/login.js
@@ -1,6 +1,23 @@
+import "./auth.css"
+
 const Login = () => {
     return (
-        <h1>Login Page</h1>
+        <div className="Login-Page">
+            <div className="Login-Page-Title">
+                <h1>Nice to see you!</h1>
+                <p>Enter your email and password to sign in</p>
+            </div>
+            <form className="Auth-Form-Input-Section">
+                <div className="Auth-Form-Input">
+                    <label for="email">Email</label>
+                    <input name="email" type="email" placeholder="Email..." />    
+                </div>
+                <div className="Auth-Form-Input">
+                    <label for="password">Password</label>
+                    <input name="password" type="password" placeholder="Password..." />
+                </div>
+            </form>
+        </div>
     )
 }
 
