53a13ceb27e5422f3156b65055429771027eb7a3
Bobur Yusupov <bobur.yu@anur.tech>
2023-11-23 12:59:23 +0500

    SideBar changes

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,5 +1,6 @@
 import routes from "../../routes/dashboard-routes";
 import "./SideBar.css";
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
     return (
@@ -12,10 +13,10 @@ const SideBar = () => {
                 {
                     routes.map((route, k) => {
                         return (
-                            <a href={route.path} key={k} className="SideBar-Link">
+                            <Link to={route.path} key={k} className="SideBar-Link">
                                 {route.icon}
                                 {route.name}
-                            </a>
+                            </Link>
                         )
                     })
                 }
