6d14d9c84ab4ec4877116faad3c65f47475899cf
Bobur Yusupov <bobur.yu06@mail.ru>
2024-09-12 20:18:50 +0200

    Layout comleted

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,47 +1,15 @@
 // App.tsx
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { mainRoutes } from './routes/main-routes';
+import { HelmetProvider } from 'react-helmet-async';
 import NotFound from './pages/NotFound/NotFound';
-
-interface RouteWithHelmetProps {
-    name?: string;
-    component: React.FC;
-}
-
-const RouteWithHelmet: React.FC<RouteWithHelmetProps> = ({
-    // eslint-disable-next-line react/prop-types
-    name,
-    // eslint-disable-next-line react/prop-types
-    component: Component,
-}) => {
-    return (
-        <>
-            <Helmet>
-                <title>{name ? `${name} - Dabang` : 'Dabang'}</title>
-            </Helmet>
-            <Component />
-        </>
-    );
-};
+import DashboardLayout from './layouts/Dashboard';
 
 const App: React.FC = () => {
     return (
         <HelmetProvider>
             <Router>
                 <Routes>
-                    {mainRoutes.map((route, index) => (
-                        <Route
-                            key={index}
-                            path={route.path}
-                            element={
-                                <RouteWithHelmet
-                                    name={route.name}
-                                    component={route.component}
-                                />
-                            }
-                        />
-                    ))}
+                    <Route path="/" element={<DashboardLayout />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </Router>
---
+++ app/src/layouts/Dashboard.tsx
@@ -0,0 +1,58 @@
+// Dashboard.tsx
+import React from 'react';
+import styles from './Dashboard.module.css';
+import Sidebar from '../components/Sidebar/Sidebar';
+import Navbar from '../components/Navbar/Navbar';
+import { mainRoutes } from '../routes/main-routes';
+import { Route, Routes } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+
+interface RouteWithHelmetProps {
+    name?: string;
+    component: React.FC;
+}
+
+const RouteWithHelmet: React.FC<RouteWithHelmetProps> = ({
+    // eslint-disable-next-line react/prop-types
+    name,
+    // eslint-disable-next-line react/prop-types
+    component: Component,
+}) => {
+    return (
+        <>
+            <Helmet>
+                <title>{name ? `${name} - Dabang` : 'Dabang'}</title>
+            </Helmet>
+            <Component />
+        </>
+    );
+};
+
+const DashboardLayout: React.FC = () => {
+    return (
+        <div className={styles.dashboardLayout}>
+            <Sidebar />
+            <div className={styles.dashboardContent}>
+                <Navbar />
+                <div className={styles.mainContent}>
+                    <Routes>
+                        {mainRoutes.map((route, index) => (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <RouteWithHelmet
+                                        name={route.name}
+                                        component={route.component}
+                                    />
+                                }
+                            />
+                        ))}
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardLayout;
